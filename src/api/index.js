2d41e2845e17ee47e876bc8b32c47d68efd49fca
haraldurbjarni <haraldurbjarni@gmail.com>
2021-05-02 18:53:23 +0000

    fix url

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://food-waste-backend-yovba.ondigitalocean.app/api';
 //https://food-waste-backend-yovba.ondigitalocean.app/api';
 
 export const appFetch = async (url, options={}) => {
