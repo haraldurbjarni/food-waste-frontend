aa3182503b1be3107248264a06342154055c4cb5
haraldurbjarni <haraldurbjarni@gmail.com>
2021-05-04 14:44:12 +0000

    fix folder upload

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -3,10 +3,44 @@ import { NextPage } from "../components/nextPage/NextPage.jsx";
 import { PreviousPage } from "../components/previousPage/PreviousPage";
 import AnimatedPage from "../components/animatedPage";
 import { Link } from "react-router-dom";
-
-const API_URL = "http://127.0.0.1:5000/";
+import {CSVLink, CSVDownload} from 'react-csv';
+import Papa from 'papaparse';
+import React, { useEffect, useRef, useState } from 'react';
 
 export function Upload() {
+
+ // const [perdayData, setPerdayData] = useState(null);
+ // const [priceData, setPriceData] = useState(null);
+//
+//
+ // const getData = async (filename) => {
+ //   const data = Papa.parse(await fetchCsv(filename));
+ //   return data;
+ // };
+ // 
+ // const fetchCsv = async (filename) => {
+ //   const response = await fetch(`files/${filename}`);
+ //   const reader = response.body.getReader();
+ //   const result = await reader.read();
+ //   const decoder = new TextDecoder('utf-8');
+ //   const csv = decoder.decode(result.value);
+ //   return csv;
+ // };
+//
+ // useEffect(() => {
+ //   let isMounted = true;
+ //   const getDat = async () => {
+ //   const perDay = await getData('exapmle_perday_sales.csv');
+ //   const price = await getData('exapmle_prices.csv');
+ //   console.log('perday', perDay.data);
+ //   if (isMounted) {
+ //     setPerdayData(perDay.data);
+ //     setPriceData(price.data);
+ //     }
+ //   } 
+ //   getDat();
+ //   return () => {isMounted = false};
+ // });
   return (
     <AnimatedPage>
       <h3>Uploading the data</h3>
@@ -21,8 +55,8 @@ export function Upload() {
         denote the number of products sold for each day for each product.
       </p>
       <p>
-        If you dont have any files to use here is an example file you can use: <Link to = "example_perday_sales.csv" target="_blank" download>example_perday_sales.csv</Link> and upload it.
-      </p>
+        If you dont have any files to use here is an example file you can use: <a href = 'files/exapmle_perday_sales.csv' downlaod>example_perday_sales.csv</a> and upload it.
+      </p>   
       <img src="first_file.PNG" />
       <File type={"sales"} />
       <h4>The second file</h4>
@@ -33,12 +67,14 @@ export function Upload() {
         above.
       </p>
       <p>
-        If you are using the perday sales example file you will have to use this one along with it: <Link to = "example_prices.csv" target="_blank" download>example_prices.csv</Link>. Dowload this and upload it.
+        If you are using the perday sales example file you will have to use this one along with it: <a href = 'files/exapmle_prices.csv' donwlaod> example_prices.csv</a>. Dowload this and upload it.
       </p>
       <img src="second_file.PNG" />
       <File type={"prices"} />
       <PreviousPage href = {"/index"}/>
       <NextPage href={"/dataOverview"} />
     </AnimatedPage>
-  );
-}
+  );}
+
+  //exapmle_perday_sales
+  //exapmle_perday_sales
